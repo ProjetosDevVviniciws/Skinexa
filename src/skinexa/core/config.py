c86294ce3a0df939d1180da94421cedcb16b83ea
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-08-02 22:20:57 -0300

    Adiciona gerenciamento de configurações por variáveis de ambiente

---
+++ src/skinexa/core/config.py
@@ -0,0 +1,45 @@
+# Configurações da aplicação Flask, incluindo a chave secreta e o banco de dados
+
+import os
+from datetime import timedelta
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    """Configurações base do Skinexa."""
+
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    STEAM_API_KEY = os.getenv("STEAM_API_KEY")
+    STEAM_OPENID_URL = os.getenv("STEAM_OPENID_URL")
+    STEAM_OPENID_REALM = os.getenv("STEAM_OPENID_REALM")
+    STEAM_OPENID_RETURN_URL = os.getenv(
+        "STEAM_OPENID_RETURN_URL"
+    )
+
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = False
+
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=8)
+
+
+class DevelopmentConfig(Config):
+    """Configurações básicas para  o ambiente de desenvolvimento."""
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+
+class ProductionConfig(Config):
+    """Configurações básicas para o ambiente de produção."""
+    DEBUG = False
+    SESSION_COOKIE_SECURE = True
+
+
+class TestingConfig(Config):
+    """Configurações básicas para o ambiente de testes."""
+    TESTING = True
+    SESSION_COOKIE_SECURE = False
+    WTF_CSRF_ENABLED = False
