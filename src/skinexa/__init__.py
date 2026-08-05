f2ecb05085b2c35c839d0ca6a037cc89d6737081
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-08-04 22:58:59 -0300

    Configura a fábrica da aplicação para múltiplos ambientes

--- src/skinexa/__init__.py
+++ src/skinexa/__init__.py
@@ -5,19 +5,24 @@ from skinexa.core.config import DevelopmentConfig
 from skinexa.core.extensions import login_manager, csrf
 from skinexa.blueprints.home.routes import home_bp
 from skinexa.blueprints.auth.routes import auth_bp
+from skinexa.blueprints.dashboard.routes import dashboard_bp
 
-def create_app():
+def create_app(
+    config_class: type = DevelopmentConfig,
+) -> Flask:
     """Cria e configura a aplicação Flask."""
     app = Flask(__name__)
     
-    _configure_app(app)
+    app.config.from_object(config_class)
+    
+    _configurar_app(app)
     _inicializar_extensoes(app)
-    #_configurar_autenticacao()
+    _configurar_autenticacao()
     _registrar_blueprints(app)
 
     return app
 
-def _configure_app(app: Flask) -> None:
+def _configurar_app(app: Flask) -> None:
     """Carrega as configurações da aplicação."""
 
     app.config.from_object(DevelopmentConfig)
@@ -33,6 +38,7 @@ def _registrar_blueprints(app: Flask) -> None:
 
     app.register_blueprint(home_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(dashboard_bp)
     
 def _configurar_autenticacao() -> None:
     """Configura toda a autenticação da aplicação."""
