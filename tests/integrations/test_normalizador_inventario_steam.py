9c5056ed7371ada6bfafde8f2782ad7912f72245
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-09-21 19:51:33 -0300

    Adiciona teste de normalização de adesivos aplicados ao item

--- tests/integrations/test_normalizador_inventario_steam.py
+++ tests/integrations/test_normalizador_inventario_steam.py
@@ -72,6 +72,160 @@ def criar_inventario_teste() -> InventarioSteamBruto:
         ),
     )
 
+def criar_inventario_com_adesivos() -> InventarioSteamBruto:
+    """Cria inventário Steam com adesivos aplicados."""
+
+    return InventarioSteamBruto(
+        steam_id=STEAM_ID_TESTE,
+        app_id=730,
+        contexto_id=2,
+        total_informado=1,
+        ativos=(
+            {
+                "appid": 730,
+                "contextid": "2",
+                "assetid": "52314340648",
+                "classid": "242017550",
+                "instanceid": "8583567976",
+                "amount": "1",
+            },
+        ),
+        descricoes=(
+            {
+                "appid": 730,
+                "classid": "242017550",
+                "instanceid": "8583567976",
+                "market_hash_name": (
+                    "M4A1-S | Nitro (Factory New)"
+                ),
+                "name": "M4A1-S | Nitro",
+                "type": "Rifle (Restrito)",
+                "tradable": 1,
+                "marketable": 1,
+                "commodity": 0,
+                "icon_url": "imagem-m4a1-s",
+                "tags": [
+                    {
+                        "category": "Weapon",
+                        "localized_tag_name": "M4A1-S",
+                    },
+                    {
+                        "category": "Exterior",
+                        "localized_tag_name": (
+                            "Nova de Fábrica"
+                        ),
+                    },
+                    {
+                        "category": "Rarity",
+                        "localized_tag_name": "Restrito",
+                    },
+                ],
+                "descriptions": [
+                    {
+                        "name": "exterior_wear",
+                        "type": "html",
+                        "value": (
+                            "Exterior: Nova de Fábrica"
+                        ),
+                    },
+                    {
+                        "name": "sticker_info",
+                        "type": "html",
+                        "value": (
+                            '<br><div id="sticker_info">'
+                            "<center>"
+                            '<img width="64" height="48" '
+                            'src="https://cdn.steamstatic.com/'
+                            'getright.png" '
+                            'title="Adesivo: GeT_RiGhT | '
+                            'Colônia 2015">'
+                            '<img width="64" height="48" '
+                            'src="https://cdn.steamstatic.com/'
+                            'nip.png" '
+                            'title="Adesivo: Ninjas in '
+                            'Pyjamas | Colônia 2015">'
+                            "<br>"
+                            "Adesivo: GeT_RiGhT | "
+                            "Colônia 2015, "
+                            "Ninjas in Pyjamas | "
+                            "Colônia 2015"
+                            "</center>"
+                            "</div>"
+                        ),
+                    },
+                ],
+                "actions": [
+                    {
+                        "link": (
+                            "steam://run/730//"
+                            "+csgo_econ_action_preview%20"
+                            "%propid:6%"
+                        ),
+                        "name": "Inspecionar no jogo...",
+                    },
+                ],
+            },
+        ),
+    )
+
+def test_normalizar_adesivos_do_inventario():
+    """Testa a normalização de adesivos aplicados ao item."""
+
+    inventario = criar_inventario_com_adesivos()
+
+    resultado = normalizar_inventario_steam(
+        inventario
+    )
+
+    assert len(resultado) == 1
+
+    item = resultado[0]
+
+    assert item.catalogo.nome_mercado == (
+        "M4A1-S | Nitro (Factory New)"
+    )
+
+    assert len(item.acessorios) == 2
+
+    primeiro = item.acessorios[0]
+
+    assert primeiro.tipo_acessorio == "adesivo"
+    assert primeiro.nome_exibicao == (
+        "GeT_RiGhT | Colônia 2015"
+    )
+    assert primeiro.url_icone == (
+        "https://cdn.steamstatic.com/getright.png"
+    )
+    assert primeiro.posicao == 0
+    assert primeiro.desgaste is None
+    assert primeiro.fonte_dados == "steam"
+
+    segundo = item.acessorios[1]
+
+    assert segundo.tipo_acessorio == "adesivo"
+    assert segundo.nome_exibicao == (
+        "Ninjas in Pyjamas | Colônia 2015"
+    )
+    assert segundo.url_icone == (
+        "https://cdn.steamstatic.com/nip.png"
+    )
+    assert segundo.posicao == 1
+    assert segundo.desgaste is None
+    assert segundo.fonte_dados == "steam"
+    assert primeiro.identificador_externo is None
+    assert primeiro.nome_mercado is None
+    assert primeiro.variante is None
+    assert primeiro.torneio is None
+    assert primeiro.equipe is None
+    assert primeiro.jogador is None
+    assert primeiro.raridade is None
+
+    assert primeiro.desgaste is None
+    assert primeiro.rotacao is None
+    assert primeiro.escala is None
+    assert primeiro.deslocamento_x is None
+    assert primeiro.deslocamento_y is None
+
 def test_normalizar_item_do_inventario():
     """Teste que valida a normalização de um item do inventário da Steam."""
     inventario = criar_inventario_teste()
@@ -108,7 +262,8 @@ def test_normalizar_item_do_inventario():
     assert "1001" in (
         item.instancia.link_inspecao or ""
     )
-
+    assert item.acessorios == ()   
+    
 def test_rejeitar_asset_sem_descricao():
     """Teste que valida que um ativo sem descrição correspondente
     é rejeitado durante a normalização do inventário da Steam."""
